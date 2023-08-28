ee1d9baa92977263a3a32271a426a16572c201cb
milind-highlevel <milind@gohighlevel.com>
2023-08-28 16:01:55 +0530

    update: exported task types

--- lib/schedulers.dart
+++ lib/schedulers.dart
@@ -1,8 +1,9 @@
 // SPDX-FileCopyrightText: (c) 2021 Artsiom iG <github.com/rtmigo>
 // SPDX-License-Identifier: MIT
 
-export 'src/c_interval.dart';
-export 'src/c_rate_limiting.dart';
-export 'src/c_lazy.dart';
-export 'src/c_time.dart';
+export 'src/b_base.dart' hide CancelFunc, QueueExt;
 export 'src/c_concurrent.dart';
+export 'src/c_interval.dart';
+export 'src/c_lazy.dart';
+export 'src/c_rate_limiting.dart';
+export 'src/c_time.dart';
